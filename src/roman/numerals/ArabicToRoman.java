ebb72447568f455c9c1a8336c3459b3b0f90a348
Neil Hanlon <neil.hanlon@ros.gov.uk>
2018-06-19 09:11:25 +0100

    better solution

--- src/roman/numerals/ArabicToRoman.java
+++ src/roman/numerals/ArabicToRoman.java
@@ -1,23 +1,20 @@
 package roman.numerals;
 
 public class ArabicToRoman {
-    private static String [] romanNumerals = {"IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
-    private static int [] arabicIntegers = {4,5,9,10,40,50,90,100,400,500,900,1000};
+    private static String [] romanNumerals = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+    private static int [] arabicIntegers = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
     public static String convert(int arabic) {
         StringBuilder sb = new StringBuilder();
         int remainder = arabic;
         for(int i= arabicIntegers.length-1; i >= 0; i--) {
             remainder = appendRomanNumeralReturnRemainder(remainder,arabicIntegers[i],romanNumerals[i], sb);
         }
-        for (int i = 1; i <= remainder; i++) {
-            sb.append("I");
-        }
         return sb.toString();
     }
     
     private static int appendRomanNumeralReturnRemainder(int arabic, int value, String romanNumeral, StringBuilder sb) {
         int remainder = arabic;
-        if(arabic >= value) {
+        while(remainder >= value) {
             remainder-=value;
             sb.append(romanNumeral);
         } 
